726c64b06a710e449fd4a35ad55c5277df386bd7
nsonu398 <nsonu398@gmail.com>
2023-03-27 16:43:28 +0530

    c-2

--- lib/models/RecentFile.dart
+++ lib/models/RecentFile.dart
@@ -48,3 +48,55 @@ List demoRecentFiles = [
     size: "34.5mb",
   ),
 ];
+
+List demoRecentFiles_2 = [
+  {
+    "icon": "assets/icons/xd_file.svg",
+    "title": "XD File",
+    "date": "01-03-2021",
+    "size": "3.5mb",
+  },
+  {
+    "icon": "assets/icons/Figma_file.svg",
+    "title": "Figma File",
+    "date": "27-02-2021",
+    "size": "19.0mb",
+  },
+  {
+    "icon": "assets/icons/doc_file.svg",
+    "title": "Document",
+    "date": "23-02-2021",
+    "size": "32.5mb",
+  },
+  {
+    "icon": "assets/icons/sound_file.svg",
+    "title": "Sound File",
+    "date": "21-02-2021",
+    "size": "3.5mb",
+  },
+  {
+    "icon": "assets/icons/xd_file.svg",
+    "title": "XD File",
+    "date": "01-03-2021",
+    "size": "3.5mb",
+  },
+  {
+    "icon": "assets/icons/Figma_file.svg",
+    "title": "Figma File",
+    "date": "27-02-2021",
+    "size": "19.0mb",
+  },
+  {
+    "icon": "assets/icons/doc_file.svg",
+    "title": "Document",
+    "date": "23-02-2021",
+    "size": "32.5mb",
+  },
+  {
+    "icon": "assets/icons/sound_file.svg",
+    "title": "Sound File",
+    "date": "21-02-2021",
+    "size": "3.5mb",
+  },
+];
+
